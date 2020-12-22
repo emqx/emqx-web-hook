c3dd93367ee74ce9a381b95d5c788bdea78ce7bb
zhouzb <zhouzb@emqx.io>
2020-12-22 14:15:19 +0800

    fix/dialyzer: update spec for function

--- src/emqx_web_hook_actions.erl
+++ src/emqx_web_hook_actions.erl
@@ -158,7 +158,7 @@ on_resource_destroy(ResId, #{<<"pool">> := PoolName}) ->
     end.
 
 %% An action that forwards publish messages to a remote web server.
--spec(on_action_create_data_to_webserver(Id::binary(), #{}) -> fun((Msg :: map()) -> any())).
+-spec on_action_create_data_to_webserver(binary(), map()) -> {[{atom(), term()}], map()}.
 on_action_create_data_to_webserver(Id, Params) ->
     #{method := Method,
       path := Path,
